90d98b5ac3013c731756d60ef63d89936a1a5f2f
PatrickDCullen <cullenpatrickd@gmail.com>
2025-03-29 12:51:44 -0400

    feat: sort required dev Composer libraries by downloads

--- app/Services/PhpService.php
+++ app/Services/PhpService.php
@@ -44,7 +44,26 @@ class PhpService
     public function getDevLibraries() : string
     {
         $composerJson = json_decode(file_get_contents($this->path . '/composer.json'));
-        $requiredPhpDevLibraries = collect($composerJson->{'require-dev'})->keys()->all();
-        return implode(', ', $requiredPhpDevLibraries);
+        $requiredPhpDevLibraries = collect($composerJson->{'require-dev'})->keys();
+
+        $sortedLibraries = collect();
+        $requiredPhpDevLibraries->each(function ($library) use ($sortedLibraries) {
+            $downloads = Http::get("https://packagist.org/packages/{$library}/stats.json")->json(
+                "downloads.monthly"
+            );
+
+            $libraryWithDownloads = ["library" => $library, "downloads" => $downloads];
+            if (! is_null($libraryWithDownloads["downloads"])) {
+                $sortedLibraries->prepend($libraryWithDownloads);
+            }
+        });
+
+        $sortedLibraries = $sortedLibraries->sortBy(function (array $package) {
+            return $package["downloads"] * -1;
+        });
+
+        $librariesByDownloads = $sortedLibraries->pluck("library");
+
+        return implode(', ', $requiredPhpDevLibraries->toArray());
     }
 }
