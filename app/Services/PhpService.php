2e0c2cb5a14fb49647b242a2fc9fb7829d731b89
PatrickDCullen <cullenpatrickd@gmail.com>
2025-03-29 12:48:05 -0400

    feat: sort required Composer libraries by downloads

--- app/Services/PhpService.php
+++ app/Services/PhpService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Http;
+
 class PhpService
 {
     // Takes the absolute path to a given project
@@ -16,8 +18,27 @@ class PhpService
     public function getLibraries() : string
     {
         $composerJson = json_decode(file_get_contents($this->path . '/composer.json'));
-        $requiredPhpLibraries = collect($composerJson->require)->keys()->all();
-        return implode(', ', $requiredPhpLibraries);
+        $requiredPhpLibraries = collect($composerJson->require)->keys();
+
+        $sortedLibraries = collect();
+        $requiredPhpLibraries->each(function ($library) use ($sortedLibraries) {
+            $downloads = Http::get("https://packagist.org/packages/{$library}/stats.json")->json(
+                "downloads.monthly"
+            );
+
+            $libraryWithDownloads = ["library" => $library, "downloads" => $downloads];
+            if (! is_null($libraryWithDownloads["downloads"])) {
+                $sortedLibraries->prepend($libraryWithDownloads);
+            }
+        });
+
+        $sortedLibraries = $sortedLibraries->sortBy(function (array $package) {
+            return $package["downloads"] * -1;
+        });
+
+        $librariesByDownloads = $sortedLibraries->pluck("library");
+
+        return implode(', ', $librariesByDownloads->toArray());
     }
 
     public function getDevLibraries() : string
