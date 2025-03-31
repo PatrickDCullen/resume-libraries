c5804af5fe96106ea5a727b39f937b289746dd25
PatrickDCullen <cullenpatrickd@gmail.com>
2025-03-31 16:41:33 -0400

    chore: refactor to projects service class

--- app/Console/Commands/GetLibraries.php
+++ app/Console/Commands/GetLibraries.php
@@ -3,15 +3,12 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Str;
-use Illuminate\Support\Facades\Storage;
+use App\Services\ProjectsService;
 use App\Services\PhpService;
 use App\Services\NpmService;
-use function Laravel\Prompts\text;
 use function Laravel\Prompts\info;
 use function Laravel\Prompts\note;
 use function Laravel\Prompts\warning;
-use function Laravel\Prompts\confirm;
 use function Laravel\Prompts\spin;
 
 class GetLibraries extends Command
@@ -35,24 +32,11 @@ class GetLibraries extends Command
      */
     public function handle()
     {
-        $projectsPath = $this->getProjectsPath();
-
-        $disk = Storage::build([
-            "driver" => "local",
-            "root" => $projectsPath
-        ]);
-
-        $projects = collect($disk->directories("/"))->filter(function ($project) {
-            // Remove this project from the projects array
-            return $project !== Str::of(base_path())
-                ->remove(Str::beforeLast(base_path(), "/") . "/")
-                ->value();
-        });
-
-        $projects->each(function ($projectDir) use ($projectsPath) {
+        $projectsService = new ProjectsService;
+        $projectsService->getProjectDirectories()->each(function ($projectDir) use ($projectsService) {
             // For now, don't worry about repetition - merging will come later
             note("Getting PHP and JavaScript dependencies for " . $projectDir);
-            $phpService = new PhpService($projectsPath . "/" . $projectDir);
+            $phpService = new PhpService($projectsService->getProjectsPath() . "/" . $projectDir);
             if ($phpService->composerJsonExists()) {
                 spin(
                     message: 'Getting data from Packagist API...',
@@ -67,7 +51,7 @@ class GetLibraries extends Command
                 warning("No composer.json found, skipping.");
             }
 
-            $npmService = new NpmService($projectsPath . "/" . $projectDir);
+            $npmService = new NpmService($projectsService->getProjectsPath() . "/" . $projectDir);
             $packageJson = $npmService->getPackageJson();
             if ($packageJson) {
                 $npmService->printLibraries();
@@ -76,33 +60,5 @@ class GetLibraries extends Command
             note("Done getting PHP and JavaScript dependencies for " . $projectDir);
         });
 
-    }
-
-    private function getProjectsPath()
-    {
-        $inferredProjectsPath = Str::of(base_path())
-            ->remove("/" . Str::afterLast(base_path(), "/"))
-            ->value();
-
-        $confirmed = confirm(
-            label: 'Is this the folder that contains your projects? ' . $inferredProjectsPath,
-            default: true,
-            yes: 'Yes',
-            no: 'No, let me change it',
-            hint: 'Not sure? Navigate to the project directory in your terminal and use the pwd command.'
-        );
-
-        $projectsPath = $inferredProjectsPath;
-
-        if (! $confirmed) {
-            $projectsPath = text(
-                label: 'Enter the absolute path of the folder that contains your projects: ',
-                placeholder: $inferredProjectsPath,
-                default: $inferredProjectsPath,
-                hint: 'Not sure? Navigate to the project directory in your terminal and use the pwd command.'
-            );
-        }
-
-        return $projectsPath;
     }
 }
---
+++ app/Services/ProjectsService.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+use function Laravel\Prompts\confirm;
+use function Laravel\Prompts\text;
+
+class ProjectsService
+{
+    private $projectsPath;
+
+    public function __construct()
+    {
+        $this->projectsPath = $this->getProjectsPathInput();
+    }
+
+    public function getProjectDirectories()
+    {
+         $projectsDirectory = Storage::build([
+             "driver" => "local",
+             "root" => $this->projectsPath
+         ]);
+
+         return collect($projectsDirectory->directories("/"))->filter(function ($project) {
+             // Remove this project from the projects array
+             return $project !== Str::of(base_path())
+                 ->remove(Str::beforeLast(base_path(), "/") . "/")
+                 ->value();
+         });
+    }
+
+    public function getProjectsPath()
+    {
+        return $this->projectsPath;
+    }
+
+    private function getProjectsPathInput()
+    {
+        $inferredProjectsPath = Str::of(base_path())
+            ->remove("/" . Str::afterLast(base_path(), "/"))
+            ->value();
+
+        $confirmed = confirm(
+            label: 'Is this the folder that contains your projects? ' . $inferredProjectsPath,
+            default: true,
+            yes: 'Yes',
+            no: 'No, let me change it',
+            hint: 'Not sure? Navigate to the project directory in your terminal and use the pwd command.'
+        );
+
+        $projectsPath = $inferredProjectsPath;
+
+        if (! $confirmed) {
+            $projectsPath = text(
+                label: 'Enter the absolute path of the folder that contains your projects: ',
+                placeholder: $inferredProjectsPath,
+                default: $inferredProjectsPath,
+                hint: 'Not sure? Navigate to the project directory in your terminal and use the pwd command.'
+            );
+        }
+
+        return $projectsPath;
+    }
+}
